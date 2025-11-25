768ef69e319ef7d49adcf6b5970cc05a4768cd49
adnanabib <cadnanyare242@gmail.com>
2025-11-25 14:28:18 +0300

    submiting exercise2

---
+++ exercise2/main.js
@@ -0,0 +1,21 @@
+
+
+let a=15;
+let b="15";
+//euqaal//
+console.log(a==b)
+//not eqaul//
+console.log(a!=b)
+
+//stricktly equal//
+console.log(a===b)
+//stricktly not equal//
+console.log(a!==b)
+//greeter then
+console.log(a>b)
+//less then
+console.log(a<b)
+//greater or equal
+console.log(a>=b)
+//less then or equal
+console.log(a<=b)
